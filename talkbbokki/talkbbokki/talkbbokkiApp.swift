fe6994b4eefc1ea99bd31c6f7b90a14d64671ef6
gabriel.jeong <wjdgo813@naver.com>
2023-01-23 21:57:33 +0900

    init

---
+++ talkbbokki/talkbbokki/talkbbokkiApp.swift
@@ -0,0 +1,17 @@
+//
+//  talkbbokkiApp.swift
+//  talkbbokki
+//
+//  Created by haehyeon.jeong on 2023/01/23.
+//
+
+import SwiftUI
+
+@main
+struct talkbbokkiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
